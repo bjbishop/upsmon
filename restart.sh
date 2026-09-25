4adfae62398562145544fb01b847c76ef399dad5
Bryan Bishop <bbishop@fastmail.com>
2026-09-25 03:21:18 +0100

    Add restart delay

--- restart.sh
+++ restart.sh
@@ -5,7 +5,9 @@ sudo systemctl stop nut-monitor
 sudo systemctl stop nut-server
 sudo systemctl stop nut-driver@myups.service
 sudo systemctl stop upsmon-bridge.service
+sleep 5
 sudo systemctl start upsmon-bridge.service
 sudo systemctl start nut-driver@myups.service
 sudo systemctl start nut-server
+sleep 5
 sudo systemctl start nut-monitor
